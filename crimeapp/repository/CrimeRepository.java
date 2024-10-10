030cd6c2dd70f95ff39a6e45a37475aa4f4c24cd
N MADHU <2000032208@kluniversity.in>
2024-10-10 10:31:13 +0530

    Delete crimeapp directory

--- crimeapp/repository/CrimeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crimeapp.repository;
-
-import com.example.crimeapp.model.Crime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrimeRepository extends JpaRepository<Crime, Long> {
-}
--- crimeapp/repository/CriminalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crimeapp.repository;
-
-import com.example.crimeapp.model.Criminal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CriminalRepository extends JpaRepository<Criminal, Long> {
-}
